7c5901b8a0ef0f70c98067d85a4da8956817bf4b
Par Bhatia <parshantx@hotmail.com>
2018-07-17 18:13:52 -0400

    //

--- CC3K/main.cc
+++ CC3K/main.cc
@@ -1,13 +1,5 @@
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+    Grid g;
 }
-
-//test alex
-
-//test par
-
-//commit 2
