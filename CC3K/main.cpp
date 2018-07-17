0bb590b2796778dcd4ab74554bdf93c30348517a
Par Bhatia <parshantx@hotmail.com>
2018-07-17 16:38:14 -0400

    Initial Commit

---
+++ CC3K/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CC3K
+//
+//  Created by Parshant Bhatia on 2018-07-17.
+//  Copyright © 2018 Parshant Bhatia. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
