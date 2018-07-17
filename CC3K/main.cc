4064cd6799803a34897da5747425d1617dea0653
Par Bhatia <parshantx@hotmail.com>
2018-07-17 19:44:48 -0400

    td changes

--- CC3K/main.cc
+++ CC3K/main.cc
@@ -1,7 +1,9 @@
 #include <iostream>
+#include "grid.h"
 
 int main(int argc, const char * argv[]) {
     Grid g;
+    cout << g;
 }
 
 
--- CC3K/textdisplay.cc
+++ CC3K/textdisplay.cc
@@ -4,10 +4,10 @@
 TextDisplay::TextDisplay() {}
 
 std::ostream &operator<<(std::ostream &out, const TextDisplay &g) {
-    for (int i=0; i<30; ++i) {
+    /*for (int i=0; i<30; ++i) {
         for (j=0; j<79; ++j) {
             cells[i][
         }
-    }
+    }*/
     return out;
 }
