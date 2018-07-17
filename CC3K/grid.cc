326539712a58dddf404d07e7bf674235047c0fbc
Par Bhatia <parshantx@hotmail.com>
2018-07-17 19:42:13 -0400

    //

--- CC3K/grid.cc
+++ CC3K/grid.cc
@@ -5,12 +5,16 @@
 #include "grid.h"
 using namespace std;
 
-void Grid::default_layout(string s) {
-    ifstream file(s.c_str());
+Grid::Grid() {
+    read_layout("cc3k_emptyfloor.txt");
+}
+
+void Grid::read_layout(string s) {
+    ifstream file(s.c_str()); //read lines
     string l;
     int row = 0;
     while(getline(file,l)) {
-        stringstream line{l};
+        stringstream line{l}; //read chars from lines
         ++row;
         char c;
         int col = 0;
@@ -18,23 +22,33 @@ void Grid::default_layout(string s) {
             ++col;
             switch(c) {
                 case '|':
-                    cells[row][col] = new OBDCell(row,col,c);
+                    cells[row][col] = new NullCell(row,col,c);
                 case '-':
-                    cells[line_num][char_num] = new OBDCell(row,col,c);
+                    cells[line_num][char_num] = new NullCell(row,col,c);
                 case '.':
                     cells[line_num][char_num] = new Floor(row,col);
                 case '#':
                     cells[line_num][char_num] = new Bridge(row,col);
                 case ' ':
-                    cells[line_num][char_num] = new OBDCell(row,col,c);
+                    cells[line_num][char_num] = new NullCell(row,col,c);
                 case '+':
                     cells[line_num][char_num] = new Door(row,col);
             }
         }
         col = 0; //reset column counter
-    }
+    } 
+    
 }
 
 std::ostream &Grid::operator<<(std::ostream &out, const Grid &g) {
+    for (int i=0; i<30; ++i) {
+        for (j=0; j<79; ++j) {
+            out << cells[i][j].print();
+        }
+        out << endl;
+    }
+    out << endl;
+    return out;
     
+    //(*td) << out; for tomorrow
 }
--- CC3K/textdisplay.h
+++ CC3K/textdisplay.h
@@ -1,6 +1,19 @@
 #ifndef textdisplay_hpp
 #define textdisplay_hpp
-
 #include <iostream>
+#include <iostream>
+#include <vector>
+using namespace std;
 
-#endif /* textdisplay_hpp */
+class Grid;
+
+class TextDisplay {
+    int size;
+    std::vector <std::vector <char>> cells;
+public:
+    TextDisplay();
+};
+
+std::ostream &operator<<(std::ostream &out, const TextDisplay &g);
+
+#endif
