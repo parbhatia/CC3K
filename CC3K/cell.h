baf2f2119389b00fa2dca604532604255a38c6b2
alexkung1 <alkung95@gmail.com>
2018-07-17 18:14:29 -0400

    cell header file

--- CC3K/cell.h
+++ CC3K/cell.h
@@ -3,13 +3,24 @@
 #include <vector>
 #include <iostream>
 
+class TextDisplay;
+class Object;
 class Cell {
     int row, col;
     TextDisplay *td;
     std::vector<std::vector<Cell*>> observers;
 public:
-    void move_to(Cell )
-}
+    void move_to(Cell& to);
+    bool accept_move(Cell &who);
+    void interact(Cell &target);
+    void accept_interact(Cell &whoFrom);
+    std::string print();
+    void notifyObservers();
+    void notify(Cell &whoFrom);
+    Object& getObject();
+    void attachObserver(Cell* ob);
+    
+};
 
 
 
