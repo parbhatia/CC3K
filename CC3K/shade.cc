5ea171a36df2979140dd11edf784adb46c65d044
Haoran <haoranhuang1997@gmail.com>
2018-07-19 17:02:24 -0400

    s

---
+++ CC3K/shade.cc
@@ -0,0 +1,21 @@
+#include "shade.h"
+
+char Shade::print() {
+    return 'S';
+}
+
+void Shade::acceptInteract(Object *whoFrom) {
+    whoFrom->interact(this);
+}
+
+void Shade::interact(Character *whoTo) {
+    whoTo->changeHp(getAtk() - whoTo->getDef());
+}
+
+void Shade::interact(Character *whoTo) {
+    
+}
+void interact(Halfling* h) {}
+void interact(Dwarf* d) {}
+void interact(Drow* h) {}
+void interact(Goblin* g) {}
