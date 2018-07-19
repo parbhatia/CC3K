ffdd2ced7e8e24711e92878b437b510e1735665c
Haoran <haoranhuang1997@gmail.com>
2018-07-19 16:57:25 -0400

    s

--- CC3K/object.h
+++ CC3K/object.h
@@ -1,6 +1,7 @@
 #ifndef OBJECT_H_
 #define OBJECT_H_
-
+class Item;
+class Character;
 class Halfling;
 class Dwarf;
 class Goblin;
@@ -9,8 +10,9 @@ class Drow;
 class Object {
 public:
     virtual char print()=0;
-    virtual void interact(Object *whoTo)=0;
-    virtual void acceptInteract(Object *whoFrom)=0; 
+    virtual void acceptInteract(Object *whoFrom)=0;
+    virtual void interact(Item *whoTo)=0;
+    virtual void interact(Character *whoTo)=0; 
     virtual void interact(Halfling* h);
     virtual void interact(Dwarf* d);
     virtual void interact(Drow* h);
