3400733fc8574b9c1fad3eb510b4aefc3d1b4d00
Haoran <haoranhuang1997@gmail.com>
2018-07-19 16:36:06 -0400

    player

--- CC3K/player.h
+++ CC3K/player.h
@@ -1,8 +1,11 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 #include "character.h"
+#include <string>
 
 class Player : public Character
 {
+public:
+    Player(std::string race="Shade", int atk=10, int def=5, int hp=100): Character{race, atk, def, hp} {}
 };
 #endif
