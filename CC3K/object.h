2297492425dfc12b3f26e1b4bddf6ec51f3f1901
Haoran <haoranhuang1997@gmail.com>
2018-07-20 17:33:17 -0400

    kh

--- CC3K/object.h
+++ CC3K/object.h
@@ -10,15 +10,15 @@ class Drow;
 
 class Object {
 public:
-  virtual char print() = 0;
-  virtual void beAttacked(Object *whoFrom) = 0;
-  virtual void attack(Item *whoTo) = 0;
-  virtual void attack(Character *whoTo) = 0;
+  virtual char print()=0;
+  virtual void beAttacked(Object *whoFrom)=0;
+  virtual void attack(Item *whoTo)=0;
+  virtual void attack(Character *whoTo)=0;
   virtual void attack(Halfling *h);
   virtual void attack(Dwarf *d);
   virtual void attack(Drow *h);
   virtual void attack(Goblin *g);
-  virtual void beUsed(Player *whoFrom);
+  virtual void beUsed(Player *whoFrom)=0;
 };
 
 #endif
