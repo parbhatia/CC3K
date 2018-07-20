9867dc50e854f1027b12cc15d25218acc131fe61
Haoran <haoranhuang1997@gmail.com>
2018-07-20 17:10:28 -0400

    cell

--- CC3K/cell.h
+++ CC3K/cell.h
@@ -11,12 +11,6 @@ class Cell
     int row, col;
     TextDisplay *td;
     std::vector<Cell *> observers;
-
-  protected:
-    Object *player = nullptr;
-    Object *ob = nullptr;
-
-  public:
 protected:
     Object* player = nullptr;
     Object* ob = nullptr;
@@ -43,18 +37,6 @@ public:
     //    virtual void notifyDisplay();
     //
     Object* getPlayer();
-//    virtual void move_to(Cell& to);
-//    virtual bool accept_move(Cell &who);
-//    virtual void interact(Cell &target);
-//    virtual void accept_interact(Cell &whoFrom);
-//
-//    virtual void notifyObservers();
-//    virtual void notify(Cell &whoFrom);
-//    virtual Object& getObject();
-//    virtual void attachObserver(Cell* ob);
-//    virtual void setDisplay(TextDisplay *td);
-//    virtual void notifyDisplay();
-//
 
 
     virtual ~Cell() = 0;
