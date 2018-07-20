d6f0aa765abbe056a62eb9a3f5052b7bab154b48
alexkung1 <alkung95@gmail.com>
2018-07-20 18:57:22 -0400

    all my shit

--- CC3K/item.cc
+++ CC3K/item.cc
@@ -1,1 +1,7 @@
 #include "item.h"
+
+char Item::print(){}
+void Item::attack(Item *whoTo){}
+void Item::attack(Character *whoTo){}
+void Item::beUsed(Player *whoFrom){}
+void Item::beAttacked(Object *whoFrom){}
--- CC3K/item.h
+++ CC3K/item.h
@@ -1,7 +1,15 @@
 #ifndef item_hpp
 #define item_hpp
 #include <iostream>
+#include "object.h"
 
+class Item:public Object{
+  virtual char print();
+  virtual void beAttacked(Object *whoFrom);
+  virtual void attack(Item *whoTo);
+  virtual void attack(Character *whoTo);
+  virtual void beUsed(Player *whoFrom);
+};
 
 
 
