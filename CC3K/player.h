6b509d40fe38417efb01d259818096b52fd99916
Haoran <haoranhuang1997@gmail.com>
2018-07-20 17:37:35 -0400

    sjdfa

--- CC3K/player.h
+++ CC3K/player.h
@@ -5,14 +5,6 @@
 
 class Player : public Character {
 public:
-    Player(std::string race="Shade", int atk=10, int def=5, int hp=100): Character{race, atk, def, hp} {}
-    void beAttacked(Object *whoFrom) override { 
-        whoFrom->attack(this);
-    }
-    void attack(Item *whoTo) override{} //Attack an item is not a valid command, so it does nothing.
-    void attack(Character *whoTo) override {
-        whoTo->changeHp(getAtk() - whoTo->getDef());
-    }
-    void beUsed(Player *whoFrom) {}; //Only a potion can be used or a gold can be picked, so it does nothing.
+    Player(std::string race, int atk, int def, int hp): Character{race, atk, def, hp} {}
 };
 #endif
