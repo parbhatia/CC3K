1e06eba426dff66c3129068bf65ca1c42e4d022e
Haoran <haoranhuang1997@gmail.com>
2018-07-22 14:16:34 -0400

    s

---
+++ CC3K/character.cc
@@ -0,0 +1,20 @@
+#include "character.h"
+    
+Character::Character(int atk, int def, int hp) : atk{atk}, def{def}, hp{hp} {}
+void Character::changeAtk(int n) {
+    atk += n;
+}
+void Character::changeDef(int n) {
+    def += n;
+}
+void Character::changeHp(int n) {
+    hp += n;  
+}
+int Character::getAtk() {return atk;}
+int Character::getDef() {return def;}
+int Character::getHp() {return hp;}
+void Character::attack(Item *whoTo) {} //Attack an item is not a valid command, so it does nothing.
+void Character::attack(Character *whoTo) {
+    whoTo->changeHp(getAtk() - whoTo->getDef());
+}
+void Character::beUsed(Player *whoFrom) {}; //Only a potion can be used or a gold can be picked, so it does nothing.ss
