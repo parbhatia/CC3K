eb2bdda116090af1146928f48650d6bba5d28e7e
Haoran <haoranhuang1997@gmail.com>
2018-07-22 15:51:15 -0400

    s

--- CC3K/player.h
+++ CC3K/player.h
@@ -4,7 +4,9 @@
 #include <string>
 
 class Player : public Character {
+    int gold;
+    bool merchant_hostile;
 public:
-    Player( int atk=10, int def=5, int hp=100): Character{atk, def, hp} {}
+    Player( int atk=10, int def=5, int hp=100): Character{atk, def, hp}, gold{0}, merchant_hostile(false) {}
 };
 #endif
