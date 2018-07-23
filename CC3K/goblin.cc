e97077a5d17e4cff51325dd3f0783f0db732824e
Haoran <haoranhuang1997@gmail.com>
2018-07-23 16:09:52 -0400

    s

--- CC3K/goblin.cc
+++ CC3K/goblin.cc
@@ -10,18 +10,35 @@ void Goblin::beAttacked(Object *whoFrom) {
   whoFrom->attack(this);
 }
 
+void Goblin::attack(Character *whoTo) {
+    int chance = rand() % 10 + 1;
+    if (chance > getMissChance()) {
+        whoTo->changeHp(-getAtk() * 100 / (100 + whoTo->getDef()));
+        if (whoTo->getHp() < 0) {
+          changeGold(5); //Goblin steals 5 gold from every slain enemy.
+        } 
+    }
+}
+
 void Goblin::attack(Halfling *h) {
   srand(time(NULL));
   int chance = rand() % 10 + 1;
   if (chance > 5) {   // Halfling has 50% chance to cause PC to miss.
-    h->changeHp(-getAtk() * 100 / (100 + h->getDef())); 
+    h->changeHp(-getAtk() * 100 / (100 + h->getDef()));
+    if (h->getHp() < 0) {
+      changeGold(5); //Goblin steals 5 gold from every slain enemy.
+    } 
   }
 }
+
 void Goblin::attack(Dwarf *d) {
   srand(time(NULL));
   int chance = rand() % 10 + 1;
   if (chance > getMissChance()) {
-    d->changeHp(-getAtk() * 100 / (100 + d->getDef())); 
+    d->changeHp(-getAtk() * 100 / (100 + d->getDef()));
+    if (d->getHp() < 0) {
+      changeGold(5); //Goblin steals 5 gold from every slain enemy.
+    } 
   }
 }
 
