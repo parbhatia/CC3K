9c81c0efa968eb2d7832d99807be01ac230618f8
Haoran <haoranhuang1997@gmail.com>
2018-07-23 19:54:48 -0400

    sa

--- CC3K/cc files/drow.cc
+++ CC3K/cc files/drow.cc
@@ -1,6 +1,7 @@
 #include "drow.h"
 #include "halfling.h"
 #include "dwarf.h"
+#include "attack.h"
 #include <cstdlib>
 
 Drow::Drow(int atk, int def, int hp): Player{atk, def, hp} {}
@@ -11,15 +12,32 @@ void Drow::beAttacked(Object *whoFrom) {
 
 void Drow::attack(Halfling *h) {
   int chance = rand() % 10 + 1;
+
   if (chance > 5) {   // Halfling has 50% chance to cause PC to miss.
-    h->changeHp(-getAtk() * 100 / (100 + h->getDef())); 
+    int dmg = (-getAtk() * 100 / (100 + h->getDef()));
+    h->changeHp(dmg);
+    if(h->getHp()<=0){
+      throw Attack{dmg, 0, Result::death};
+    } else {
+      throw Attack{dmg, h->getHp(), Result::attack};
+    } 
+  } else {
+    throw Attack{0, h->getHp(), Result::miss};
   }
 }
 
 void Drow::attack(Dwarf *d) {
   int chance = rand() % 10 + 1;
   if (chance > getMissChance()) {
-    d->changeHp(-getAtk() * 100 / (100 + d->getDef())); 
+    int dmg = (-getAtk() * 100 / (100 + d->getDef()));
+    d->changeHp(dmg); 
+    if(d->getHp()<=0){
+            throw Attack{dmg, 0, Result::death};
+    } else {
+      throw Attack{dmg, d->getHp(), Result::attack};
+    }    
+  } else {
+    throw Attack{0, d->getHp(), Result::miss};
   }
 }
 
--- CC3K/goblin.cc
+++ CC3K/goblin.cc
@@ -1,6 +1,7 @@
 #include "goblin.h"
 #include "halfling.h"
 #include "dwarf.h"
+#include "attack.h"
 #include <cstdlib>
 
 Goblin::Goblin(): Player{15, 20, 110} {}
@@ -12,30 +13,48 @@ void Goblin::beAttacked(Object *whoFrom) {
 void Goblin::attack(Character *whoTo) {
     int chance = rand() % 10 + 1;
     if (chance > getMissChance()) {
-        whoTo->changeHp(-getAtk() * 100 / (100 + whoTo->getDef()));
-        if (whoTo->getHp() < 0) {
+        int dmg = (-getAtk() * 100 / (100 + whoTo->getDef()));
+        whoTo->changeHp(dmg);
+        if (whoTo->getHp() <= 0) {
           changeGold(5); //Goblin steals 5 gold from every slain enemy.
+          throw Attack{dmg, 0 , Result::death};
+        } else {
+          throw Attack{dmg, whoTo->getHp(), Result::attack};
         } 
+    } else {
+          throw Attack{0, whoTo->getHp(), Result::miss};
     }
 }
 
 void Goblin::attack(Halfling *h) {
   int chance = rand() % 10 + 1;
   if (chance > 5) {   // Halfling has 50% chance to cause PC to miss.
-    h->changeHp(-getAtk() * 100 / (100 + h->getDef()));
-    if (h->getHp() < 0) {
+    int dmg = (-getAtk() * 100 / (100 + h->getDef()));
+    h->changeHp(dmg);
+    if (h->getHp() <= 0) {
       changeGold(5); //Goblin steals 5 gold from every slain enemy.
+      throw Attack{dmg, 0, Result::death};
+    } else {
+      throw Attack{dmg, h->getHp(), Result::attack};
     } 
-  }
+  } else {
+      throw Attack{0, h->getHp(), Result::miss};
+  } 
 }
 
 void Goblin::attack(Dwarf *d) {
   int chance = rand() % 10 + 1;
   if (chance > getMissChance()) {
-    d->changeHp(-getAtk() * 100 / (100 + d->getDef()));
-    if (d->getHp() < 0) {
+    int dmg = (-getAtk() * 100 / (100 + d->getDef()));
+    d->changeHp(dmg);
+    if (d->getHp() <= 0) {
       changeGold(5); //Goblin steals 5 gold from every slain enemy.
-    } 
+      throw Attack{dmg, 0, Result::death};
+    } else {
+      throw Attack{dmg, d->getHp(), Result::attack};
+    }
+  } else {
+     throw Attack{0, d->getHp(), Result::miss};
   }
 }
 
