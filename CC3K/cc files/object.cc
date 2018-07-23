7fc54c9a60ced5a789dbd1633d14a1ffa23e2888
Haoran <haoranhuang1997@gmail.com>
2018-07-23 17:23:36 -0400

    gf

--- CC3K/cc files/object.cc
+++ CC3K/cc files/object.cc
@@ -1,4 +1,9 @@
-//
 #include "object.h"
 
 Object::~Object() {}
+void Object::attack(Halfling *h) {}
+void Object::attack(Dwarf *d) {}
+void Object::attack(Drow *h) {}
+void Object::attack(Goblin *g) {}
+void Object::attack(Drow *h) {}  //enemy
+void Object::attack(Goblin *g) {}  //enemy
