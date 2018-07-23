8d3306c95c66b41b639a17d6c22cf8bcca54d426
Haoran <haoranhuang1997@gmail.com>
2018-07-23 16:27:21 -0400

    d

--- CC3K/cc files/drow.cc
+++ CC3K/cc files/drow.cc
@@ -10,14 +10,14 @@ void Drow::beAttacked(Object *whoFrom) {
 }
 
 void Drow::attack(Halfling *h) {
-  int chance = rand()%10 + 1;
+  int chance = rand() % 10 + 1;
   if (chance > 5) {   // Halfling has 50% chance to cause PC to miss.
     h->changeHp(-getAtk() * 100 / (100 + h->getDef())); 
   }
 }
 
 void Drow::attack(Dwarf *d) {
-  int chance = rand()%10 + 1;
+  int chance = rand() % 10 + 1;
   if (chance > getMissChance()) {
     d->changeHp(-getAtk() * 100 / (100 + d->getDef())); 
   }
