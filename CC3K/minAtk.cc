e7996cbb01c2ec886cb620d11008ae0835510557
Haoran <haoranhuang1997@gmail.com>
2018-07-24 16:55:52 -0400

    j

--- CC3K/minAtk.cc
+++ CC3K/minAtk.cc
@@ -1,8 +1,12 @@
 #include "minAtk.h"
 #include "player.h"
+#include "attack.h"
 
 void MinAttack::beUsed(Player *whoFrom){
+    double before = whoFrom->getAtk();
     whoFrom->changeAtk(-5*whoFrom->getPotRate());
+    double after = whoFrom->getAtk();
+    throw Potion{before,after,Type::decreaseATK};
 }
 
 MinAttack::~MinAttack(){}
