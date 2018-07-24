49b683e4224c3b13e41c5e3140c62c7359ff807d
Haoran <haoranhuang1997@gmail.com>
2018-07-24 16:25:16 -0400

    dlml

--- CC3K/attack.h
+++ CC3K/attack.h
@@ -1,8 +1,15 @@
 
 enum Result {attack, death, miss};
+enum Type {increaseATK, decreaseATK, increaseDEF, decreaseDEF, increaseHP, decreaseHP};
 
-struct Attack{
+struct Attack {
     double dmg;
     double health;
     Result res; 
+};
+
+struct Potion {
+    double before;
+    double after;
+    Type type;
 };
